47386ee86a7ffecdd12ffb4f69d01308316fef6c
jesusMedina20 <jesus.amb0241@gmail.com>
2025-08-19 10:05:56 -0400

    add homepage

---
+++ src/app/form/page.tsx
@@ -0,0 +1,11 @@
+import Form from '@/components/form'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex items-center justify-center min-h-screen p-16'>
+      <Form />
+
+    </div>
+  )
+}
---
+++ src/types/bannerConfig/index.ts
@@ -0,0 +1,6 @@
+export interface BannerConfig {
+  title: string;
+  subtitle: string;
+  button: string;
+  src: string;
+}
